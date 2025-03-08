18c0245a9de129dc1254b064bdde42da6c987f7f
Helen Burgess <helen.burgess@starlingbank.com>
2025-03-08 13:42:44 +0000

    add sql file to build database

--- data/UserPreferences.sql
+++ data/UserPreferences.sql
@@ -5,7 +5,12 @@ USE dwellr;
 CREATE TABLE UserPreferences (
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
-    preferences TEXT[]
+    value INT,
+    location TEXT,
+    no_of_bedrooms INT,
+    is_new_home BOOLEAN,
+    has_garden BOOLEAN,
+    features TEXT
 );
 
 CREATE TABLE Users (
@@ -25,6 +30,9 @@ INSERT INTO Users (id, username, password, email, first_name, last_name)
         (3, harrypotter, harrypotterpassword, harrypotter@dwellr.com, Harry, Potter);
         (4 ,hermionegranger, hermionegrangerpassword, hermionegranger@dwellr.com, Hermione, Granger);
 
-INSERT INTO UserPreferences (id, user_id, preferences)
+INSERT INTO UserPreferences (id, user_id, value, location, no_of_bedrooms, is_new_home, has_garden, features)
     VALUES
-        (1, 1, )
+        (1, 1, 500000, 'London', 3, true, true, 'Garage'),
+        (2, 2, 300000, 'Manchester', 2, false, false, 'Basement Storage'),
+        (3, 3, 1000000, 'London', 4, true, true, 'Solar Panels'),
+        (4, 4, 200000, 'Manchester', 1, false, false, 'Garage');
