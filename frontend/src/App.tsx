db469550fe39963617fad23b8f3cc6c5f4b67549
Emily White <emily.white@starlingbank.com>
2025-03-08 15:10:10 +0000

    preferences

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css';
 import { Box, Flex, Button } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
-import { getProperties } from './api';
+// import { useQuery } from '@tanstack/react-query';
+// import { getProperties } from './api';
 import { ReactComponent as Logo } from './assets/Dwellr_Logo.svg';
 
 function App() {
-  const { isLoading, isError, data } = useQuery({ queryKey: ['todos'], queryFn: getProperties });
+  // const { isLoading, isError, data } = useQuery({ queryKey: ['todos'], queryFn: getProperties });
 
-  console.log('isLoading', isLoading);
-  console.log('isError', isError);
-  console.log(data);
 
   return (
     <Flex
@@ -19,13 +16,12 @@ function App() {
       padding={4}
       alignItems="center"
       justifyContent="center"
-      bg="brandPrimary"
+      bg="white"
       gap={4}
     >
-      <Box as={Logo} color="textContrast" />
-      <Button variant="contrast">Log in</Button>
+      <Box as={Logo} color="brandPrimary" />
       <Button variant="primary">Log in</Button>
-      <Button variant="secondary">Log in</Button>
+      <Button variant="secondary">Register</Button>
     </Flex>
   );
 }
--- frontend/src/Preferences.tsx
+++ frontend/src/Preferences.tsx
@@ -1,7 +1,92 @@
-import { Box } from '@chakra-ui/react';
+import {
+  FormLabel,
+  Flex,
+  FormControl,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+  CheckboxGroup,
+  Checkbox
+} from '@chakra-ui/react';
+
+// price range - number
+// bedrooms - number
+// isflat - boolean
+// isnewhome - boolean
+// hasgarden - boolean
+// features - string[]
+// location - string
 
 const Preferences = () => {
-  return <Box>PREFERENCES</Box>;
+  return (
+    <Flex
+      as="main"
+      flexDirection="column"
+      minHeight="100vh"
+      padding={4}
+      alignItems="center"
+      justifyContent="center"
+      bg="white"
+      gap={6}
+    >
+      <FormControl>
+        <FormLabel>Max price</FormLabel>
+        <Input type="number" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Number of bedrooms</FormLabel>
+        <Input type="number" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Property type</FormLabel>
+        <RadioGroup>
+          <Stack direction="row">
+            <Radio value="flat">Flat</Radio>
+            <Radio value="house">House</Radio>
+          </Stack>
+        </RadioGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>New home</FormLabel>
+        <RadioGroup>
+          <Stack direction="row">
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </Stack>
+        </RadioGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Garden</FormLabel>
+        <RadioGroup>
+          <Stack direction="row">
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </Stack>
+        </RadioGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Features</FormLabel>
+        <CheckboxGroup>
+          <Stack direction="row">
+            <Checkbox value="1">1</Checkbox>
+            <Checkbox value="2">2</Checkbox>
+            <Checkbox value="3">3</Checkbox>
+          </Stack>
+        </CheckboxGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Location</FormLabel>
+        <Input type="text" />
+      </FormControl>
+    </Flex>
+  );
 };
 
 export default Preferences;
