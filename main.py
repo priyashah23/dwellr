1567f4ae23231e9f688e0896a4cd3b0202265a6e
Priya Shah <priya.shah2@starlingbank.com>
2025-03-08 12:43:23 +0000

    Make basic API endpoints (No functionality)

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/property", methods=['GET']) # We will need an id of the property and the user
+def get_property(): 
+    property_dict = {"num_of_bedrooms": 3}
+    return property_dict
+
+
+@app.route("user/<user_id>", methods=['GET'])
+def get_quiz_question():
+    pass
+
+@app.route("user/<user_id>/preferences", methods=['POST'])
+def post_preferences():
+    pass
+
+@app.route("user/<user_id>/preferences", methods=['PUT'])
+def update_preferences():
+    pass
+
