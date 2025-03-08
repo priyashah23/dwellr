15e596c7c12edc43af2bbc31c1cc8ab5dc68478a
Priya Shah <priya.shah2@starlingbank.com>
2025-03-08 23:05:35 +0000

    Add preferences that match with the db

--- app/Preferences.py
+++ app/Preferences.py
@@ -1,5 +1,8 @@
 from PurchaseProperty import PurchaseProperty
 
 class Preferences:
-    def __init__(self, properties : list)
-        self.properties = properties
+    def __init__(self, max_price: int, number_of_beds: int, is_new_home: bool, location: str):
+        self.max_price = max_price
+        self.number_of_beds = number_of_beds
+        self.is_new_home = is_new_home
+        self.location =  location
